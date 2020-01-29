a6a1201c13d98cd149d5988081e2166cefe3bcf3
amilissa <57662726+amilissa@users.noreply.github.com>
2020-01-29 15:08:36 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::any('exercise-1', 'Exercise1');
-Route::any('exercise-2', 'Exercise2');
-Route::any('exercise-3', 'Exercise3@index');
-Route::any('exercise-3/store', 'Exercise3@store');
-Route::any('exercise-final', 'ExerciseFinal@login');
-Route::any('exercise-final/store', 'ExerciseFinal@store');
+Route::any('amilissa-ex1', 'Exercise1');
+Route::any('amilissa-ex2', 'Exercise2');
+Route::any('amilissa-ex3', 'Exercise3@index');
+Route::any('amilissa-ex3/store', 'Exercise3@store');
+Route::any('amilissa-final', 'ExerciseFinal@login');
+Route::any('amilissa-final/store', 'ExerciseFinal@store');
